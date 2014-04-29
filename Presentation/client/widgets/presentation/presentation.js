f81fc8935b6854214cd4cf1164a5bde3a777f49d
Sergey Yuferev <nexor@ya.ru>
2014-04-29 09:14:04 +0400

    fixes

--- Presentation/client/widgets/presentation/presentation.js
+++ Presentation/client/widgets/presentation/presentation.js
@@ -1,6 +1,7 @@
 var Presenteration,
     R      = require('renderer'),
-    Slides = require('slides');
+    Slides = require('slides'),
+    Navigation = require('navigation');
 
 Presentation = function (props) {
   this.props = props;
@@ -24,11 +25,16 @@ proto.componentDidMount = function () {
 
 };
 
+proto.onPrev = function () {
+
+};
+
 
 proto.render = function() {
   return [
     R('h2', {innerHTML: 'Презентация № ' + this.props.number,  className: 'presenter'}),
-    R(Slides, {list: this.state.slides})
+    R(Slides, {list: this.state.slides}),
+    R(Navigation, {onPrev: this.onNavigationPrev.bind(this), onNext: this.onNavigationNext.bind(this)})
   ];
 };
 
